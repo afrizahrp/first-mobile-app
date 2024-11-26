fbc2c41343dd87e6dab31d25b60a7a9237a56328
afrizaharahap <Afriza.harahap@gmail.com>
2024-11-26 15:08:13 +0700

    initial-commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,16 @@
+// This is the main file of the app. It is the entry point of the app.
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RootLayout from './_layout';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        <RootLayout />
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
